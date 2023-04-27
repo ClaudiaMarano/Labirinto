660214f6e52b35ebca55d792bb1235297ac1048a
ClaudiaMarano <118067217+ClaudiaMarano@users.noreply.github.com>
2023-04-27 16:19:04 +0200

    Schema iniziale codice

---
+++ Calcolatore.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+class Calcolatore:
+    
+    """
+    determinare per ogni punto di partenza il percorso che permette di raggiungere il punto di arrivo
+    o, in alternativa, se non esiste alcun percorso possibile.
+    
+    Nel caso esista un percorso che permette di raggingere da un punto di partenza il punto di arrivo, 
+    oltre al percorso, il programma deve anche fornire il suo costo totale che è la somma dei costi 
+    incontrati lungo di esso più la lunghezza del percorso in pixel.
+    
+    Nel caso esistano più percorsi possibili da un determinato punto di partenza, 
+    il programma deve fornire il percorso con il costo minore.
+    
+    
+    Il risultato finale deve essere fornito attraverso:
+
+    un'immagine su cui è indicato il percorso con un colore diverso per ogni punto di partenza;
+
+    un file JSON associato all'immagine con l'informazione del punteggio raggiunto da ogni percorso o, 
+    in alternativa, da un messaggio nel caso non esista un percorso possibile da qualcuno delle posizioni di partenza.
+
+    """
+    
+    def esiste_percorso():
+        """
+        Boolean:
+            True se esiste il percorso
+            False se non esiste il percorso
+
+        Returns
+        -------
+        None.
+
+        """
+        
+    def lunghezza_percorso():
+        """
+        
+
+        Returns
+        -------
+        None.
+
+        """
+    def costo_percorso():
+        """
+        
+        Returns
+        -------
+        None.
+
+        """
+    def percorso_costo_minore():
+        """
+        
+
+        Returns
+        -------
+        None.
+
+        """
+        
+    def lunghezza_pixel():
+        """
+    
+
+        Returns
+        -------
+        None.
+
+        """
+    
+
---
+++ main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Il programma deve acquisire il layout di un labirinto costituito da una matrice di posizioni, una o più posizioni di partenza 
+e una posizione di arrivo, e determinare per ogni punto di partenza il percorso che permette di raggiungere 
+il punto di arrivo o, in alternativa, se non esiste alcun percorso possibile.
+
+"""
+
+def get_partenza():
+    """
+    
+
+    Returns
+    -------
+    None.
+
+    """
+    
+def get_arrivo():
+    """
+    
+
+    Returns
+    -------
+    None.
+
+    """
+
