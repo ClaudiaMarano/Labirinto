1cc5dab18884a80be377c8d57b60a66756ef9f15
ClaudiaMarano <118067217+ClaudiaMarano@users.noreply.github.com>
2023-04-28 11:22:31 +0200

    Aggiunta classe di output

---
+++ output_file.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Il risultato finale deve essere fornito attraverso:
+
+- un'immagine su cui è indicato il percorso con un colore diverso per ogni punto di partenza;
+
+- un file JSON associato all'immagine con l'informazione del punteggio raggiunto da ogni percorso
+  o, in alternativa, da un messaggio nel caso non esista un percorso possibile da qualcuno 
+  delle posizioni di partenza.
+  
+"""
+
+class output_file:
+    
+    """
+    Restituisce i file di output
+    
+    
+    """
